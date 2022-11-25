dc2f531caa870554b3c4c0e24d09031f98257c6f
Wagner Alvarado <warnerxp@outlook.com>
2022-11-24 20:24:01 -0600

    Oblivion

--- NotifyFrontEnd/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NotifyFrontEnd.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
